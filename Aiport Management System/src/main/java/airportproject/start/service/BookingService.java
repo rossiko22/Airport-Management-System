f41c70f48c58c89c07ed6b577f9a0786e0a5978e
Marko Spasovski <markospasovskq@gmail.com>
2024-06-01 14:23:11 +0200

    delete booking , just add to make the airplane seat to false after deleting and amke sure in flight delete it also deletes all bookings related to it

--- Aiport Management System/src/main/java/airportproject/start/service/BookingService.java
+++ Aiport Management System/src/main/java/airportproject/start/service/BookingService.java
@@ -2,12 +2,15 @@ package airportproject.start.service;
 
 import airportproject.start.dto.BookingDto;
 import airportproject.start.entity.Booking;
+import airportproject.start.entity.Flight;
 import airportproject.start.repo.BookingRepo;
+import airportproject.start.repo.FlightRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -15,6 +18,10 @@ public class BookingService {
 
     @Autowired
     BookingRepo bookingRepository;
+
+    @Autowired
+    FlightRepo flightRepository;
+
 
     public List<BookingDto> getBookingByFlightsId(Long flightId) {
         List<BookingDto> booking = bookingRepository.findBookingByFlightsId(flightId);
